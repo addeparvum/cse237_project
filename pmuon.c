4e0579d7e7f063dc7b35da48a4d84bcce3c34119
chengchenzhang <addeparvum@gmail.com>
2019-01-28 16:34:37 -0800

    i will turn to the lord

--- pmuon.c
+++ pmuon.c
@@ -38,24 +38,24 @@ void set_pmu(void* dummy) {
 
     // 5. Set six event counter registers (Project Assignment you need to IMPLEMENT)
 	// Instructions Architechurally executed
-	asm volatile("mcr p15, 0, %0, c9, c12, 5\n\t" ::"r"(0x00));
+	asm volatile("mrc p15, 0, %0, c9, c12, 5\n\t" ::"r"(0x00));
 	ptype = INST_RETIRED;
-	asm volatile("mcr p15, 0, %0, c9, c13, 1\n\t" ::"r"(INST_RETIRED));	
+	asm volatile("mrc p15, 0, %0, c9, c13, 1\n\t" ::"r"(INST_RETIRED));	
 
 	//L1D Cache Access
-	asm volatile("mcr p15, 0, %0, c9, c12, 5\n\t" ::"r"(0x01));
+	asm volatile("mrc p15, 0, %0, c9, c12, 5\n\t" ::"r"(0x01));
 	ptype = L1D_CACHE;
-	asm volatile("mcr p15, 0, %0, c9, c13, 1\n\t" ::"r"(L1D_CACHE));
+	asm volatile("mrc p15, 0, %0, c9, c13, 1\n\t" ::"r"(L1D_CACHE));
 	
 	//L1D Cache Miss
-	asm volatile("mcr p15, 0, %0, c9, c12, 5\n\t" ::"r"(0x02));
+	asm volatile("mrc p15, 0, %0, c9, c12, 5\n\t" ::"r"(0x02));
 	ptype = L1D_CACHE_M;
-	asm volatile("mcr p15, 0, %0, c9, c13, 1\n\t" ::"r"(L1D_CACHE_M));
+	asm volatile("mrc p15, 0, %0, c9, c13, 1\n\t" ::"r"(L1D_CACHE_M));
 	
 	//L2D Cache Access
-	asm volatile("mcr p15, 0, %0, c9, c12, 5\n\t" ::"r"(0x03));
+	asm volatile("mrc p15, 0, %0, c9, c12, 5\n\t" ::"r"(0x03));
 	ptype = L2D_CACHE;
-	asm volatile("mcr p15, 0, %0, c9, c13, 1\n\t" ::"r"(L2D_CACHE));
+	asm volatile("mrc p15, 0, %0, c9, c13, 1\n\t" ::"r"(L2D_CACHE));
 
 	//L2D Cache Miss
 	asm volatile("mcr p15, 0, %0, c9, c12, 5\n\t" ::"r"(0x04));
