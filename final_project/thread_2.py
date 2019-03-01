020d9e6c61c4c853e3457aacfb4b6bcb336e399a
chengchenzhang <addeparvum@gmail.com>
2019-02-28 20:12:52 -0800

    added thread2

---
+++ final_project/thread_2.py
@@ -0,0 +1,16 @@
+from subprocess import Popen, PIPE, STDOUT
+import pty
+import os
+
+cmd = './printOutput'
+
+master, slave = pty.openpty()
+
+p = Popen(cmd, shell=True, stdin=PIPE, stdout=slave, stderr=slave, close_fds=True)
+stdout = os.fdopen(master)
+p.stdin.write(('0\n').encode())
+p.stdin.write(('1\n').encode())
+p.stdin.write(('0\n').encode())
+p.stdin.write(('3\n').encode())
+#print stdout.readline()
+#print stdout.readline()
