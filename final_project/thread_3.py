215164a4e25836c732f11b961ddd0a3e3a535b98
Chengchen Zhang <addeparvum@gmail.com>
2019-03-01 18:04:55 +0000

    can read and write from stdin and out

--- final_project/thread_3.py
+++ final_project/thread_3.py
@@ -1,29 +1,39 @@
 from subprocess import Popen, PIPE
 from time import sleep
 from fcntl import fcntl, F_GETFL, F_SETFL
-from os import O_NONBLOCK, read
+from os import O_NONBLOCK, read, fdopen
+import pty
+
+master, slave = pty.openpty()
 
 # run the shell as a subprocess:
 p = Popen(['./printOutput'],
-        stdin = PIPE, stdout = PIPE, stderr = PIPE, shell = False)
+        stdin = PIPE, stdout = slave, stderr = slave, shell = True, bufsize=1)
+
 # set the O_NONBLOCK flag of p.stdout file descriptor:
-flags = fcntl(p.stdout, F_GETFL) # get current p.stdout flags
-fcntl(p.stdout, F_SETFL, flags | O_NONBLOCK)
+stdout = fdopen(master)
+
+flags = fcntl(stdout, F_GETFL) # get current p.stdout flags
+fcntl(stdout, F_SETFL, flags | O_NONBLOCK)
+
+
 # issue command:
 x = 1
 while x == 1:
     inputNum = int(input("Enter a number: "))
     p.stdin.write(('%d\n' % inputNum).encode())
+    if inputNum != 1 and inputNum != 0:
+        break
+        
+    sleep(0.1)
+    out = (stdout.read())[:-1]
+    print(out)
    
-    if inputNum == 4:
-        x = 0
 # let the shell output the result:
 sleep(0.1)
 # get the output
-while True:
-    try:
-        print read(p.stdout.fileno(), 1024),
-    except OSError:
-        # the os throws an exception if there is no data
-        print '[No more data]'
-        break
+try:
+    print("done!")
+except OSError:
+    # the os throws an exception if there is no data
+    print '[No more data]'
