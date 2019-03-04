427950162f7c00a1019cc8f54d35791a7bba3121
chengchenzhang <addeparvum@gmail.com>
2019-03-03 21:37:20 -0800

    testing sensors

--- final_project/server_3.py
+++ final_project/server_3.py
@@ -13,7 +13,7 @@ host_port = 80
 master, slave = pty.openpty()
 
 # run the shell as a subprocess:
-p = Popen(['./printOutput'],
+p = Popen(['./run.out'],
         stdin = PIPE, stdout = slave, stderr = slave, shell = True, bufsize=1)
 
 # set the O_NONBLOCK flag of p.stdout file descriptor:
@@ -33,16 +33,18 @@ print(out)
    
 
 class MyServer(SimpleHTTPServer.SimpleHTTPRequestHandler):
-    """ A special implementation of BaseHTTPRequestHander for reading data from
-        and control GPIO of a Raspberry Pi
-    """
+   
+        path_to_image = 'detectedface.png'
+        img = open(path_to_image,'rb')
+        statinfo = os.stat(path_to_image)
+        img_size = statinfo.st_size
+
 
     def do_HEAD(self):
-        """ do_HEAD() can be tested use curl command
-            'curl -I http://server-ip-address:port'
-        """
+     
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
+        self.send_header('content')
         self.end_headers()
 
     def _redirect(self, path):
@@ -52,53 +54,41 @@ class MyServer(SimpleHTTPServer.SimpleHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        """ do_GET() can be tested using curl command
-            'curl http://server-ip-address:port'
-        """
+        
         html = '''
            <html>
            <body style="width:960px; margin: 20px auto;">
            <h1>Welcome to my Raspberry Pi</h1>
-           <p>Current GPU temperature is {}</p>
+           <p>Box Output:{}</p>
            <form action="/" method="POST">
                Turn LED :
                <input type="submit" name="submit" value="On">
                <input type="submit" name="submit" value="Off">
            </form>
-           <img src="facedetected.jpg" alt="Italian Trulli">
+           
            </body>
            </html>
         '''
-        temp = popen("/opt/vc/bin/vcgencmd measure_temp").read()
+
+        p.stdin.write(('4\n').encode())
+        sleep(0.1)
+        temp = (stdout.read())[:-1]
+       # temp = popen("/opt/vc/bin/vcgencmd measure_temp").read()
         self.do_HEAD()
-        self.wfile.write(html.format(temp[5:]).encode("utf-8"))
+        self.wfile.write(html.format(temp).encode("utf-8"))
 
     def do_POST(self):
-        """ do_POST() can be tested using curl command
-            'curl -d "submit=On" http://server-ip-address:port'
-        """
+       
         content_length = int(self.headers['Content-Length'])  # Get the size of data
         post_data = self.rfile.read(content_length).decode("utf-8")  # Get the data
         post_data = post_data.split("=")[1]  # Only keep the value
 
-        # GPIO setup
-      #  GPIO.setmode(GPIO.BOARD)
-      #  GPIO.setwarnings(False)
-      #  GPIO.setup(12, GPIO.OUT)
-      #  p = GPIO.PWM(12, 50)
-      #  p.start(7.5)
 
         if post_data == 'On':
             p.stdin.write(('0\n').encode())
             sleep(0.1)
             out = (stdout.read())[:-1]
-            print(out)
-       #     p.ChangeDutyCycle(7.5)
-       #     time.sleep(1)
-       #     p.ChangeDutyCycle(2.5)
-       #     time.sleep(1)
-       #     p.ChangeDutyCycle(12.5)
-       #     time.sleep(1)
+      
         else:
             p.stdin.write(('1\n').encode())
             sleep(0.1)
