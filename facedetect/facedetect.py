237640cfe4a0fa84ada532e543cf3ecde2a5f345
chengchenzhang <addeparvum@gmail.com>
2019-03-03 17:39:39 -0800

    added more code

--- facedetect/facedetect.py
+++ facedetect/facedetect.py
@@ -1,4 +1,18 @@
 from imgproc import *
 
-print("faces galore")
+cam = Camera(160, 120)
+view = Viewer(cam.width, cam.height, "Face detection")
 
+while True:
+	image = cam.grabImage()
+
+	faces = image.detectFaces()
+
+	for face in faces:
+		# extract the x, y, width and height from the tuple
+		x, y, width, height = face
+
+		# use this information to draw an orange rectangle around the face
+		image.drawRect(x, y, width, height, 128, 0, 255)
+
+	view.displayImage(image)
